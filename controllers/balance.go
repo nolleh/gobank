de6237555dcbf3382406afc7e44f212bad9824f7
nolleh7707 <nolleh7707@gmail.com>
2019-08-09 15:16:50 +0900

    test

--- controllers/balance.go
+++ controllers/balance.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"time"
+	"net/http"
 	"github.com/labstack/echo"
 )
 
@@ -13,12 +14,12 @@ func (b *BalanceController) Init(g *echo.Group) {
 	g.GET("/:userId", b.Get)
 }
 
-func (b *BalanceController) Get(c *echo.Context) error {
+func (b *BalanceController) Get(c echo.Context) error {
 	var content struct {
         Response  string `json:"response"`
         Timestamp string `json:"timestamp"`
 	}
 	content.Response = "Hello, World!"
-    content.Timestamp = getNow()
+    content.Timestamp = time.Now().String()
 	return c.JSON(http.StatusOK, &content)
 }
