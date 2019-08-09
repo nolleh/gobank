aca8936dd4d1b3e0e22067b8887c13ecc190fce1
nolleh7707 <nolleh7707@gmail.com>
2019-08-09 14:43:23 +0900

    test

--- controllers/balance.go
+++ controllers/balance.go
@@ -7,12 +7,12 @@ type BalanceController struct {
 }
 
 func (c *BalanceController) Init(g *echo.group) {
-	g.GET("/:userId", c.get)
+	g.GET("/:userId", c.Get)
 }
 
-func (c *BalanceController) get(c echo.Context) {
+func (c *BalanceController) Get(c echo.Context) {
 	jsonMap := map[string]string {
-		"foo": "bar"
+		"foo": "bar",
 	}
 	return c.JSON(http.StatusOK, jsonMap)
 }
